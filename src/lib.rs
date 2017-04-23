72a73c72270b6f3594d10fb03200a193789f65cb
maiquel <jleahred@gmail.com>
2017-04-23 20:29:09 +0200

    working on grammar

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+
+extern crate indentation_flattener;
+use indentation_flattener::flatter;
+
+
+include!(concat!(env!("OUT_DIR"), "/dinpeg.rs"));
+
+
+#[test]
+fn validate_grammars() {
+    let validate = parse("main = aaaa");
+    assert!(validate == Ok(()));
+
+    let validate = parse(&flatter(r#"
+        main = def
+        def
+            = "def" _ func_name _ "(" _ params _ "):" _ eol+
+            |           body
+
+        func_name = id
+
+        id  = [A-Za-z][A-Za-z0-9_]*
+        eol = "\n"
+        _   = " "*
+
+        // body = $INDENT(stament*)
+
+        stament
+            = expr
+            / if
+
+        if
+            =  "if" _ expr _ ":" _
+            |        body
+            |  "else:" _
+            |        body
+"#)
+        .unwrap()
+        .0);
+    println!("{:?} ____________", validate);
+    assert!(validate == Ok(()));
+
 }
