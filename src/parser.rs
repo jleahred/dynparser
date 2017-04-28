199a361c27672d95ab45549d04af8b03b49b9bab
josÃ© luis <jleahred@gmail.com>
2017-04-28 22:03:28 +0200

    adding dot parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,22 @@
 #![allow(dead_code)]
 // pending... remove
 
-
 extern crate indentation_flattener;
-// use indentation_flattener::flatter;
+// use indentation_flattener::flatter
 
 
 
-fn parse(parsing: Parsing, text: &ParsingText, rules: &Rules) -> Result<Parsing, String> {
-    let expr = rules.get(&parsing.symbol).ok_or("undefined symbol")?;
+fn parse(symbol: &Symbol, parsing: Parsing, rules: &Rules) -> Result<Parsing, String> {
+    let expr = rules.get(symbol).ok_or("undefined symbol")?;
 
 
     let parsing = match expr {
-            &Expression::Terminal(ref term) => term.parse(parsing, text),
+            &Expression::Terminal(ref term) => term.parse(parsing),
             _ => Err("Pending implementation".to_owned()),
         }
         ?;
 
-    if parsing.position.n == text.0.len() {
+    if parsing.position.n == parsing.parsing_text.0.len() {
         Ok(parsing)
     } else {
         Err(format!("not consumed full input"))
@@ -25,6 +24,10 @@ fn parse(parsing: Parsing, text: &ParsingText, rules: &Rules) -> Result<Parsing,
 
 }
 
+
+fn symbol(s: &str) -> Symbol {
+    Symbol(s.to_owned())
+}
 
 
 
@@ -76,33 +79,43 @@ enum Terminal {
 
 
 impl Terminal {
-    fn parse(&self, parsing: Parsing, text: &ParsingText) -> Result<Parsing, String> {
+    fn parse(&self, parsing: Parsing) -> Result<Parsing, String> {
         match self {
-            &Terminal::Literal(ref s) => parse_literal_string(s, parsing, text),
+            &Terminal::Literal(ref s) => parse_literal_string(s, parsing),
+            &Terminal::Dot => parse_dot(parsing),
             _ => Err("pending implementation".to_owned()),
         }
     }
 }
 
-fn parse_literal_string(s: &str,
-                        mut parsing: Parsing,
-                        text: &ParsingText)
-                        -> Result<Parsing, String> {
+fn parse_literal_string(s: &str, mut parsing: Parsing) -> Result<Parsing, String> {
     let self_len = s.len();
-    let in_text = text.0.chars().skip(parsing.position.n).take(self_len).collect::<String>();
+    let in_text =
+        parsing.parsing_text.0.chars().skip(parsing.position.n).take(self_len).collect::<String>();
     if s == in_text {
         parsing.position.n += self_len;
-        parsing.position.row += self_len;
+        parsing.position.col += self_len;
         Ok(parsing)
     } else {
         Err("pending implementation".to_owned())
     }
 }
 
+fn parse_dot(mut parsing: Parsing) -> Result<Parsing, String> {
+    match parsing.position.n < parsing.parsing_text.0.len() {
+        true => {
+            parsing.position.n += 1;
+            parsing.position.col += 1;
+            Ok(parsing)
+        }
+        false => Err("expected . on end of file".to_owned()),
+    }
+}
 
 
 
-#[derive(Debug, PartialEq, Eq, Hash, Default)]
+
+#[derive(Debug, PartialEq, Eq, Hash, Default, Clone)]
 struct Symbol(String);
 
 
@@ -127,17 +140,22 @@ impl ParsingText {
 }
 
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Parsing {
     position: ParsPosition,
-    symbol: Symbol,
     parsing_text: ParsingText,
 }
 
 
 impl Parsing {
-    fn new(symbol: &str) -> Self {
-        Parsing { symbol: Symbol(symbol.to_owned()), ..Parsing::default() }
+    fn new(s: &str) -> Self {
+        Parsing { parsing_text: ParsingText(s.to_owned()), ..Parsing::default() }
+    }
+
+
+    fn set_text(mut self, text: &str) -> Self {
+        self.parsing_text = ParsingText(text.to_owned());
+        self
     }
 }
 
@@ -170,14 +188,21 @@ macro_rules! parser(
             )+
             m
         }
-     };
+    };
 );
 
 
 macro_rules! lit(
     { $value:expr } => {
         Expression::Terminal(Terminal::Literal($value.to_owned()))
-     };
+    };
+);
+
+
+macro_rules! dot(
+    { } => {
+        Expression::Terminal(Terminal::Dot)
+    };
 );
 
 
@@ -211,19 +236,38 @@ fn validate_literal() {
     let grammar = &parser!{ 
             "main" => lit!("aaaa")
     };
-    assert!(parse(Parsing::new("main"),
-                       &ParsingText::new("aaaa"),
-                       grammar).is_ok());
-    assert!(parse(Parsing::new("main"),
-                       &ParsingText::new("aaa"),
-                       grammar).is_err());
-    assert!(parse(Parsing::new("main"),
-                       &ParsingText::new("bbbb"),
-                       grammar).is_err());
-    assert!(parse(Parsing::new("main"),
-                       &ParsingText::new("aaaaa"),
-                       grammar).is_err());
-    assert!(parse(Parsing::new("main"),
-                       &ParsingText::new(""),
-                       grammar).is_err());
+    assert!(parse(&symbol("main"),
+                    Parsing::new("aaaa"),
+                    grammar).is_ok());
+    assert!(parse(&symbol("main"),
+                    Parsing::new("aaa"),
+                    grammar).is_err());
+    assert!(parse(&symbol("main"),
+                    Parsing::new("bbbb"),
+                    grammar).is_err());
+    assert!(parse(&symbol("main"),
+                    Parsing::new("aaaaa"),
+                    grammar).is_err());
+    assert!(parse(&symbol("main"),
+                    Parsing::new(""),
+                    grammar).is_err());
+}
+
+#[test]
+fn validate_dot() {
+    let grammar = &parser!{
+            "main" => dot!()
+    };
+
+    assert!(parse(&symbol("main"),
+                    Parsing::new("a"),
+                    grammar).is_ok());
+
+    assert!(parse(&symbol("main"),
+                    Parsing::new("aaa"),
+                    grammar).is_err());
+
+    assert!(parse(&symbol("main"),
+                    Parsing::new(""),
+                    grammar).is_err());
 }
